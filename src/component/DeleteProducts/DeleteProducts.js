079dd9eb3cce1fbab3aeca05e7f88dc642f32cd5
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-04-01 13:04:10 +0600

    Delete button added

--- src/component/DeleteProducts/DeleteProducts.js
+++ src/component/DeleteProducts/DeleteProducts.js
@@ -1,18 +1,30 @@
+import { Button } from '@material-ui/core';
 import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../App';
-
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import { DeleteOutline } from '@material-ui/icons';
 const DeleteProducts = () => {
     const [orders , setOrders] = useState([]);
     const [loggedInUser ,setLoggedInUser] = useContext(UserContext)
     useEffect(()=>{
-        fetch('https://peaceful-crag-13613.herokuapp.com/orderHistory?email='+loggedInUser.email)
+        fetch('https://peaceful-crag-13613.herokuapp.com/showProduct')
         .then(res=>res.json())
         .then(data => setOrders(data))
     },[])
+    const handleDelete = (id)=>{
+        console.log(id)
+        fetch(`https://peaceful-crag-13613.herokuapp.com/deleteProduct/${id}`,{
+            method : "DELETE"
+        })
+        .then(res=>res.json())
+        .then(data =>{
+            console.log("one item deleted")
+        })
+    }
     return (
             <div>
             {
-                orders.map(order => <li>{order.email}  {order.date}  {order.product}  {order.names} </li>)
+                orders.map(order => <li>Product :  {order.name} , Price :     {order.price}<Button onClick={()=>handleDelete(order._id)} color="secondary" variant="contained"><DeleteForeverIcon/></Button>  </li> )
             }
         </div>
     );
