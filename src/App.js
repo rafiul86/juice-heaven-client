e5247c160e72f983e448c29f89bc5fceb2ab7ef2
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2021-04-02 12:06:48 +0600

    icon deleted

--- src/App.js
+++ src/App.js
@@ -44,9 +44,6 @@ function App() {
       <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
            HILL-VIEW-MART
           </Typography>
